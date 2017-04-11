bf2c81b49940fbbebb6f985e49aa7a89695021c8
Joonas Forsberg <joonas.forsberg@napa.fi>
2017-04-11 08:25:11 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+   /**
+   * Configuration file for the AWAS-project.
+   *
+   * Please edit to match your environment 
+   */
+
+   /* MySQL connection */
+   $DB_NAME = "awasdb";
+   $DB_USERNAME = "username";
+   $DB_PASSWORD = "password";
+   $DB_HOST = "127.0.0.1";
+
+   /* AWAS blog configuration */
+?>
+
+
