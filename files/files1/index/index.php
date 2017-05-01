f637bfd4b0968e753a4eb19a21d635e8dfd8818d
HaiLee <duonghailee@gmail.com>
2017-05-01 22:36:41 +0300

    adding files directory

---
+++ files/files1/index/index.php
@@ -0,0 +1,45 @@
+<?php 
+    /* Show all errors */ 
+    ini_set('display_startup_errors', 1);
+    ini_set('display_errors', 1);
+
+    include('views/head.php');
+
+    /* Include generic config file */
+    include('config.php');
+
+    $conn = mysqli_connect($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
+
+    if (!$conn) {
+        echo "Error: Unable to connect to MySQL." . PHP_EOL;
+        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    }
+
+    /* Include correct page after MySQL connection has been established */
+    $page = $_GET["p"];
+    if ($page == "home" || $page == null) {
+         include('views/content.php');
+    } elseif ($page == "posts" || preg_match("/posts\&show\=\d/", $page)) {
+        include('views/posts.php');
+    } elseif ($page == "contact") {
+         include('views/contact.php');
+    } elseif ($page == "login" || $page == "Login") {
+         include('views/login.php');
+    } elseif ($page == "register") {
+        include('views/admin_panel.php');
+    } elseif ($page == "logout") {
+
+        /* Destroy session and redirect to index.php */
+        session_destroy();
+        header("Location: index.php");
+        die();
+
+    } else {
+        include('views/404.html');
+    }
+
+    /* Close Connection */
+    mysqli_close($conn);
+    include('views/footer.html');
+?>
